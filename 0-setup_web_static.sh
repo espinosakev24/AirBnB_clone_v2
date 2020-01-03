969d8d1a02ba9912eddfd488aae560eb05a9e33f
espinosakev24 <916@holbertonschool.com>
2020-01-03 16:40:03 -0500

    creates all folders for web deployment and set up the nginx conf file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 apt-get install nginx
-mkdir -p data/web_static/releases/test && touch data/web_static/releases/test/index.html
-mkdir data/web_static/shared
-if [ -L data/web_static/current ]
+mkdir -p /data/web_static/releases/test && touch /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/shared
+echo "holbertonschool" > /data/web_static/releases/test/index.html
+if [ -L /data/web_static/current ]
 then
-    rm data/web_static/current
-    ln -s data/web_static/current
+    rm /data/web_static/current
+    ln -snf /data/web_static/releases/test /data/web_static/current
 else
-    ln -s data/web_static/current
+    ln -snf /data/web_static/releases/test /data/web_static/current
 fi
 chown --recursive ubuntu:ubuntu /data
-sed -i "s/^[\t]location \/ {$/location \/hbnb_static {\n\talias \/data\/web_static\/current\/\n\tautoindex off/g;" /etc/nginx/sites-available/default
-sudo service nginx restart
+sed -i "60i location \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default
+service nginx restart
