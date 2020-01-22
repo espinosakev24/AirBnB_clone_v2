8e8f58010e95356fcbc78411fbc9921a4298230b
espinosakev24 <916@holbertonschool.com>
2020-01-21 22:00:04 -0500

    fixing task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
 #!/user/bin/python3
 from flask import Flask
+""" Script that starts flask app
+"""
 
 app = Flask('__name__')
 @app.route('/', strict_slashes=False)
 def Home():
+    """ Function of route / that displays a string
+    """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
+
