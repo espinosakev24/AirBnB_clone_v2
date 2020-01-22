989e64e6ffbc8d3e7bde7dbecfd724c36c3ba750
espinosakev24 <916@holbertonschool.com>
2020-01-21 22:25:12 -0500

    fixing task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,21 @@
 #!/user/bin/python3
 from flask import Flask
+app = Flask(__name__)
 
-app = Flask('__name__')
+
 @app.route('/', strict_slashes=False)
 def Home():
+    """ / route
+    """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def Hbnb():
+    """ hbnb route
+    """
     return "HBNB"
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
